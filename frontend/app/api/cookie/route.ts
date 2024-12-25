1c32b4db90964a0fbaf2bf779ce9b031909f5765
ahmadshehroz1 <ahmadshehroz109@gmail.com>
2024-12-25 13:57:31 +0500

    commit

--- frontend/app/api/cookie/route.ts
+++ frontend/app/api/cookie/route.ts
@@ -3,45 +3,37 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  // Retrieve cookies
   const cookieStore = await cookies();
-
   const token = cookieStore.get("jwt");
 
+  // If no token, respond with Unauthorized status
   if (!token) {
     return NextResponse.json(
-      {
-        message: "Unauthorized",
-      },
-      {
-        status: 401,
-      }
+      { message: "Unauthorized" },
+      { status: 401 }
     );
   }
 
   const { value } = token;
-
-
   const secret = process.env.JWT_SECRET || "";
 
   try {
-    const user=verify(value, secret);
+    // Verify JWT token
+    const user = verify(value, secret);
 
+    // Return user data and JWT if valid
     const response = {
       user: user,
-      jwt:value
+      jwt: value
     };
-    
-    return new Response(JSON.stringify(response), {
-      status: 200,
-    });
+
+    return new Response(JSON.stringify(response), { status: 200 });
   } catch (e) {
+    // Handle token verification failure
     return NextResponse.json(
-      {
-        message: "Something went wrong",
-      },
-      {
-        status: 400,
-      }
+      { message: "Something went wrong" },
+      { status: 400 }
     );
   }
 }
